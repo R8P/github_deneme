3e437fe5fe61e621b9dfea5f858978f83581c215
R8P <abdulkadirozkan@yandex.com>
2020-06-20 19:10:55 +0300

    addText() eklendi.

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ $(document).ready(function () {
     $btnOne.click(()=>{
         setBackground()
     })
+
+    $('#btnThree').on("click",function() {
+        addText();
+    })
 });
 
 
@@ -15,4 +19,10 @@ function setBackground() {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     $('#colorArea').css("background","#" + randomColor);
     $('#colorArea >span').html("Color:#"+randomColor);
+}
+
+function addText(){
+    const inputValue = $('#inputText').val();
+        $('#textArea').append("<p>"+ inputValue +"</p>");
+        $('#inputText').val('');
 }
